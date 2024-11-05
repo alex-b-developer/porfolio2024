1c1b7bc969e36532b3a20487db9bf101aef3f85d
ALEX B-DEV <alexbetbu@gmail.com.com>
2024-11-04 19:45:40 -0500

    :fix:implementando mejoras responsive y metadata

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import Skills from './components/skills/skills';
 import Portfolio from './components/portfolio/portfolio';
 import About from './components/about/about'
 import {NextUIProvider} from "@nextui-org/react";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
  <NextUIProvider>
+    <Helmet>
+      <title>Alexander B Developer</title>
+      <meta name="description" content="Portafolio Web Profesional. Alexander Betancur, Web Developer, Front End Developer" />
+      <meta name="keywords" content="Web Developer, Frontend Developer, React Developer, JavaScript Developer, NextJS Developer, TailwindCSS Expert, UX/UI Design, Responsive Web Design, Full Stack Development, Freelance Web Developer, HTML5, CSS3, Web Application Development, Mobile First Design, Custom Website Development, Creative Web Solutions, Web Portfolio, Software Development, WordPress Developer, Web Animation Expert, Medellín Web Developer, Colombian Web Developer, Alexander Betancur Developer" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GWZCXE7MTJ"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GWZCXE7MTJ');
+          `}
+        </script>
+    </Helmet>
     <div className="App">
       <header className="App-header">
       <a href="https://wa.me/573148734025" target="_blank" rel="noreferrer">
           DO YOU WANT TO GET A PROFESSIONAL WEBSITE? CONTACT ME NOW!</a>
-        <Showcase />
       </header>
+      <Showcase />
       <main>
         <h1 class="text-black font-medium text-2xl pt-10 pb-5">Work</h1>
         <Portfolio />
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -34,7 +34,7 @@ export default function App() {
     <div className="max-w-[1200px] gap-2 grid grid-cols-12 grid-rows-2 px-8 mx-auto">
 
       {/* Tarjeta JVICON LLC */}
-      <Card className="col-span-6 sm:col-span-4 h-[300px]">
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
             JVICON LLC
@@ -70,7 +70,7 @@ export default function App() {
       </Card>
 
       {/* Card for Drink Safety Miami Landing Page */}
-      <Card className="col-span-6 sm:col-span-4 h-[300px]">
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
             Drink Safety Miami
@@ -224,7 +224,7 @@ export default function App() {
       </Card>
 
       {/* Card for Women’s Business Enterprise */}
-      <Card className="col-span-6 sm:col-span-3 h-[300px]">
+      <Card className="col-span-12 sm:col-span-3 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
             Women’s Business Enterprise
@@ -260,7 +260,7 @@ export default function App() {
       </Card>
 
       {/* Card for Sportravel */}
-      <Card className="col-span-6 sm:col-span-3 h-[300px]">
+      <Card className="col-span-12 sm:col-span-3 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
             Sportravel
