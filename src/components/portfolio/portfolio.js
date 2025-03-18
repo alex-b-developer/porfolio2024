a168dcac3fbb9839c8ae3bb849d2b37b64ee8c45
ALEX B-DEV <alexbetbu@gmail.com.com>
2025-03-18 13:08:43 -0500

    actualizando el portafolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -10,10 +10,10 @@ import {
 // Importación de imágenes
 import drinkImg from "../../img/portfolio-drink.jpg";
 import isolutionImg from "../../img/portfolio-isolution.PNG";
-import jviconImg from "../../img/portfolio-jvicon.jpg";
-import karismaImg from "../../img/portfolio-karisma.PNG";
+import teltechImg from "../../img/portfolio-teltech.jpg";
+import thematicImg from "../../img/portfolio-thematic.jpg";
 import lagunaImg from "../../img/portfolio-laguna.jpg";
-import nickelodeonImg from "../../img/portfolio-nickelodeon.PNG";
+import tesImg from "../../img/portfolio-tes.png";
 import shoesImg from "../../img/portfolio-shoes.jpg";
 import sportravelImg from "../../img/portfolio-sportravel.jpg";
 import wbehfImg from "../../img/portfolio-wbehf.PNG";
@@ -21,10 +21,10 @@ import wbehfImg from "../../img/portfolio-wbehf.PNG";
 // Importación de íconos
 import drinkIcon from "../../img/icon-drink.PNG";
 import isolutionIcon from "../../img/icon-isolution.PNG";
-import jviconIcon from "../../img/icon-jvicon.PNG";
-import karismaIcon from "../../img/icon-karisma.PNG";
+import teltechIcon from "../../img/icon-teltech.PNG";
+import thematicIcon from "../../img/icon-thematic.PNG";
 import lagunaIcon from "../../img/icon-laguna.PNG";
-import nickelodeonIcon from "../../img/icon-nickelodeon.PNG";
+import tesIcon from "../../img/icon-tes.PNG";
 import shoesIcon from "../../img/icon-shoes.PNG";
 import sportravelIcon from "../../img/icon-sportravel.PNG";
 import wbehfIcon from "../../img/icon-wbehf.PNG";
@@ -33,35 +33,75 @@ export default function App() {
   return (
     <div className="max-w-[1200px] gap-2 grid grid-cols-12 grid-rows-2 px-8 mx-auto">
 
-      {/* Tarjeta JVICON LLC */}
-      <Card className="col-span-12 sm:col-span-4 h-[300px]">
-        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">
-            JVICON LLC
+      {/* Card for Laguna Media Group Corporate Website */}
+      <Card
+        isFooterBlurred
+        className="w-full h-[300px] col-span-12 sm:col-span-5"
+      >
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+        <p className="text-tiny text-white/60 uppercase font-bold">
+        Laguna Media Group
           </p>
-          <h4 className="text-white font-medium text-large">
-            Corporate Website
+          <h4 className="text-white/90 font-medium text-xl">
+            Corporate Website and Tech Blog
           </h4>
         </CardHeader>
         <Image
           isZoomed
           removeWrapper
-          alt="Imagen del sitio web de JVICON LLC"
+          alt="Card example background"
+          className="z-0 w-full h-full -translate-y-6 object-cover"
+          src={lagunaImg}
+        />
+        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+        <div className="flex flex-grow gap-2 items-center">
+            <Image
+              alt="Icon"
+              className="rounded-full w-10 h-11 bg-black"
+              src={lagunaIcon}
+            />
+            <div className="flex flex-col">
+              <p className="text-tiny text-white/60">lagunamg.com</p>
+            </div>
+          </div>
+          <a href="https://lagunamg.com/" target="_blank" rel="noreferrer">
+            <Button radius="full" size="sm">
+              Visit Website
+            </Button>
+          </a>
+        </CardFooter>
+      </Card>
+
+      {/* Card for Isolution Service Center Ecommerce */}
+      <Card
+        isFooterBlurred
+        className="w-full h-[300px] col-span-12 sm:col-span-7"
+      >
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">
+            Isolution Service Center
+          </p>
+          <h4 className="text-white/90 font-medium text-xl">Ecommerce</h4>
+        </CardHeader>
+        <Image
+          isZoomed
+          removeWrapper
+          alt="Relaxing app background"
           className="z-0 w-full h-full object-cover"
-          src={jviconImg}
+          src={isolutionImg}
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
             <Image
-              alt="Logo icon for JVICON LLC"
+              alt="Icon"
               className="rounded-full w-10 h-11 bg-black"
-              src={jviconIcon}
+              src={isolutionIcon}
             />
             <div className="flex flex-col">
-              <p className="text-tiny text-white/60">jviconllc.com</p>
+              <p className="text-tiny text-white/60">isolution.com.co</p>
             </div>
           </div>
-          <a href="https://jviconllc.com/" target="_blank" rel="noreferrer">
+          <a href="https://isolution.com.co/" target="_blank" rel="noreferrer">
             <Button radius="full" size="sm">
               Visit Website
             </Button>
@@ -147,75 +187,37 @@ export default function App() {
         </CardFooter>
       </Card>
 
-      {/* Card for Laguna Media Group Corporate Website */}
-      <Card
-        isFooterBlurred
-        className="w-full h-[300px] col-span-12 sm:col-span-5"
-      >
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">
-        Laguna Media Group
-          </p>
-          <h4 className="text-white/90 font-medium text-xl">
-            Corporate Website and Tech Blog
-          </h4>
-        </CardHeader>
-        <Image
-          isZoomed
-          removeWrapper
-          alt="Card example background"
-          className="z-0 w-full h-full -translate-y-6 object-cover"
-          src={lagunaImg}
-        />
-        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-        <div className="flex flex-grow gap-2 items-center">
-            <Image
-              alt="Icon"
-              className="rounded-full w-10 h-11 bg-black"
-              src={lagunaIcon}
-            />
-            <div className="flex flex-col">
-              <p className="text-tiny text-white/60">lagunamg.com</p>
-            </div>
-          </div>
-          <a href="https://lagunamg.com/" target="_blank" rel="noreferrer">
-            <Button radius="full" size="sm">
-              Visit Website
-            </Button>
-          </a>
-        </CardFooter>
-      </Card>
-
-      {/* Card for Isolution Service Center Ecommerce */}
-      <Card
-        isFooterBlurred
-        className="w-full h-[300px] col-span-12 sm:col-span-7"
-      >
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+      {/* Card for Thematic */}
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
-            Isolution Service Center
+            Thematic Luxury
           </p>
-          <h4 className="text-white/90 font-medium text-xl">Ecommerce</h4>
+          <h4 className="text-white font-medium text-large">Corporate Website</h4>
         </CardHeader>
         <Image
           isZoomed
           removeWrapper
-          alt="Relaxing app background"
+          alt="Card background"
           className="z-0 w-full h-full object-cover"
-          src={isolutionImg}
+          src={thematicImg}
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
             <Image
               alt="Icon"
               className="rounded-full w-10 h-11 bg-black"
-              src={isolutionIcon}
+              src={thematicIcon}
             />
             <div className="flex flex-col">
-              <p className="text-tiny text-white/60">isolution.com.co</p>
+              <p className="text-tiny text-white/60">thematicluxury.com</p>
             </div>
           </div>
-          <a href="https://isolution.com.co/" target="_blank" rel="noreferrer">
+          <a
+            href="https://thematicluxury.com"
+            target="_blank"
+            rel="noreferrer"
+          >
             <Button radius="full" size="sm">
               Visit Website
             </Button>
@@ -295,37 +297,35 @@ export default function App() {
         </CardFooter>
       </Card>
 
-      {/* Card for Karisma Hotels and resorts */}
+      {/* Tarjeta Teltech */}
       <Card className="col-span-12 sm:col-span-3 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
-            Karisma Hotels and Resorts
+            Teltech Group
           </p>
-          <h4 className="text-white font-medium text-large">Mailing Webpage</h4>
+          <h4 className="text-white font-medium text-large">
+            Corporate Website
+          </h4>
         </CardHeader>
         <Image
           isZoomed
           removeWrapper
-          alt="Card background"
+          alt="Imagen del sitio web de JVICON LLC"
           className="z-0 w-full h-full object-cover"
-          src={karismaImg}
+          src={teltechImg}
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
             <Image
-              alt="Icon"
+              alt="Logo icon for JVICON LLC"
               className="rounded-full w-10 h-11 bg-black"
-              src={karismaIcon}
+              src={teltechIcon}
             />
             <div className="flex flex-col">
-              <p className="text-tiny text-white/60">karismahotels.com</p>
+              <p className="text-tiny text-white/60">teltech.com</p>
             </div>
           </div>
-          <a
-            href="https://marketing.karismahotels.com/eblasts/Weeding-newsletterFeb2023/index.html"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://teltech.com/" target="_blank" rel="noreferrer">
             <Button radius="full" size="sm">
               Visit Website
             </Button>
@@ -337,30 +337,30 @@ export default function App() {
       <Card className="col-span-12 sm:col-span-3 h-[300px]">
         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
           <p className="text-tiny text-white/60 uppercase font-bold">
-            Nickelodeon Hotels and Resorts
+            Telecom Electric Supply
           </p>
-          <h4 className="text-white font-medium text-large">Mailing Webpage</h4>
+          <h4 className="text-white font-medium text-large">Corporate Website</h4>
         </CardHeader>
         <Image
           isZoomed
           removeWrapper
           alt="Card background"
           className="z-0 w-full h-full object-cover"
-          src={nickelodeonImg}
+          src={tesImg}
         />
         <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
           <div className="flex flex-grow gap-2 items-center">
             <Image
               alt="Icon"
               className="rounded-full w-10 h-11 bg-black"
-              src={nickelodeonIcon}
+              src={tesIcon}
             />
             <div className="flex flex-col">
-              <p className="text-tiny text-white/60">karismahotels.com</p>
+              <p className="text-tiny text-white/60">tes85.com</p>
             </div>
           </div>
           <a
-            href="https://marketing.karismahotels.com/eblasts/SOS-April2023/SOS-Launch-TA/index.html"
+            href="https://www.tes85.com/"
             target="_blank"
             rel="noreferrer"
           >
